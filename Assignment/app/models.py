a56cd2acc2f41f5286fa146144c85de213b0a15d
arjunbamoriya535 <arjunkushwah17@gmail.com>
2021-03-19 17:11:46 +0530

    commit
    add databse configuration  model

---
+++ Assignment/app/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+# Create your models here.
+### email = models.CharField(max_length=70, default="")
+   # phone = models.CharField(max_length=70, default="")
+
+
+    #def __str__(self):
+     #   return self.name
